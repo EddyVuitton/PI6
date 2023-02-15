82f067c7208dfe90b37a82ebf2775b5581d2b8dd
Edward Gadawski <edwardgadawski@gmail.com>
2023-02-15 21:04:35 +0100

    tworzenie quizu 1

---
+++ PI6.Components/Pages/TworzenieQuizu.razor.cs
@@ -0,0 +1,17 @@
+﻿using MudBlazor;
+
+namespace PI6.Components.Pages;
+
+public partial class TworzenieQuizu
+{
+    public string TextValue1 { get; set; }
+    public string TextValue2 { get; set; }
+
+    private int _questionCount = 1;
+
+    private void AddNew()
+    {
+        _questionCount++;
+        StateHasChanged();
+    }
+}
