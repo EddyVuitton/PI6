c282affed2dbdf6a6c4b914d70c871aec7b3c75e
Edward Gadawski <edwardgadawski@gmail.com>
2023-02-19 16:38:51 +0100

    tworzenie quizu 3

--- PI6.Components/Pages/TworzenieQuizu.razor.cs
+++ PI6.Components/Pages/TworzenieQuizu.razor.cs
@@ -1,23 +1,63 @@
-﻿using MudBlazor;
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
 
 namespace PI6.Components.Pages;
 
 public partial class TworzenieQuizu
 {
-    public string TextValue1 { get; set; }
-    public string TextValue2 { get; set; }
+    [Inject] public IJSRuntime JSRuntime { get; set; }
 
-    private int _questionCount = 1;
+    private string _title = string.Empty;
+    private List<int> _questionList = new();
+    private Dictionary<int, int> _questionOption = new();
+    private Dictionary<int, string> _questionTitle = new();
+    private Dictionary<int, string> _options = new();
 
-    private void AddQuestion()
+    private async Task AddQuestion()
     {
-        _questionCount++;
+        int questionMax = _questionList.Count == 0 ? 0 : _questionList.Max();
+        int questionId = questionMax + 1;
+        _questionList.Add(questionId);
+        _questionTitle.Add(questionId, string.Empty);
+
+        //await ConsoleLog($"Count: {_questionOption.Count}");
+
+        await AddOption(questionId, string.Empty);
+
+        //var questioOptionMax = (_questionOption.Count == 0 ? 0 : _questionOption.Max(x => x.Value)) + 1;
+        //_questionOption.Add(questioOptionMax, questionId);
+        //_options.Add(questioOptionMax, string.Empty);
+
         StateHasChanged();
     }
 
-    private void DeleteQuestion()
+    private void DeleteQuestion(int id)
     {
-        _questionCount--;
+        var options = _questionOption.Where(x => x.Key == id).ToArray();
+        foreach (var o in options)
+            _options.Remove(o.Key);
+
+        _questionOption.Remove(id);
+        _questionTitle.Remove(id);
+        _questionList.Remove(id);
+
+        StateHasChanged();
+    }
+
+    private async Task ConsoleLog(string message)
+    {
+        await JSRuntime.InvokeVoidAsync("console.log", message);
+    }
+
+    private async Task AddOption(int questionId, string optionText)
+    {
+        var questioOptionMax = (_questionOption.Count == 0 ? 0 : _questionOption.Max(x => x.Key)) + 1;
+
+        await ConsoleLog($"Max _questionOption: {(_questionOption.Count == 0 ? 0 : _questionOption.Max(x => x.Value)) + 1}");
+
+        _questionOption.Add(questioOptionMax, questionId);
+        _options.Add(questioOptionMax, optionText);
+
         StateHasChanged();
     }
 }
---
+++ PI6.WebApi/Helpers/JS.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.JSInterop;
+
+namespace PI6.WebApi.Helpers
+{
+    public class JS
+    {
+        private readonly IJSRuntime JsRuntime;
+        public JS(IJSRuntime jSRuntime)
+        {
+            this.JsRuntime = jSRuntime;
+        }
+
+        public async Task LogAsync(string message)
+        {
+            await JsRuntime.InvokeVoidAsync("console.log", message);
+        }
+    }
+}
