a186c0a7aaa8d764737bde3eaaff4affac8e50a6
Edward Gadawski <edwardgadawski@gmail.com>
2023-02-21 22:32:18 +0100

    tworzenie quizu 4

--- PI6.Components/Pages/TworzenieQuizu.razor.cs
+++ PI6.Components/Pages/TworzenieQuizu.razor.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Components;
 using Microsoft.JSInterop;
+using PI6.Components.Shared.Objects;
 
 namespace PI6.Components.Pages;
 
@@ -8,40 +9,12 @@ public partial class TworzenieQuizu
     [Inject] public IJSRuntime JSRuntime { get; set; }
 
     private string _title = string.Empty;
-    private List<int> _questionList = new();
-    private Dictionary<int, int> _questionOption = new();
-    private Dictionary<int, string> _questionTitle = new();
-    private Dictionary<int, string> _options = new();
+    private readonly List<Question> _questions = new();
+    private readonly List<QuestionOption> _questionOptions = new();
 
-    private async Task AddQuestion()
+    protected override void OnInitialized()
     {
-        int questionMax = _questionList.Count == 0 ? 0 : _questionList.Max();
-        int questionId = questionMax + 1;
-        _questionList.Add(questionId);
-        _questionTitle.Add(questionId, string.Empty);
-
-        //await ConsoleLog($"Count: {_questionOption.Count}");
-
-        await AddOption(questionId, string.Empty);
-
-        //var questioOptionMax = (_questionOption.Count == 0 ? 0 : _questionOption.Max(x => x.Value)) + 1;
-        //_questionOption.Add(questioOptionMax, questionId);
-        //_options.Add(questioOptionMax, string.Empty);
-
-        StateHasChanged();
-    }
-
-    private void DeleteQuestion(int id)
-    {
-        var options = _questionOption.Where(x => x.Key == id).ToArray();
-        foreach (var o in options)
-            _options.Remove(o.Key);
-
-        _questionOption.Remove(id);
-        _questionTitle.Remove(id);
-        _questionList.Remove(id);
-
-        StateHasChanged();
+        AddQuestion();
     }
 
     private async Task ConsoleLog(string message)
@@ -49,15 +22,78 @@ public partial class TworzenieQuizu
         await JSRuntime.InvokeVoidAsync("console.log", message);
     }
 
-    private async Task AddOption(int questionId, string optionText)
+    private void AddQuestion()
     {
-        var questioOptionMax = (_questionOption.Count == 0 ? 0 : _questionOption.Max(x => x.Key)) + 1;
+        int questionId = _questions.Count == 0 ? 1 : _questions.Max(x => x.GetId()) + 1;
 
-        await ConsoleLog($"Max _questionOption: {(_questionOption.Count == 0 ? 0 : _questionOption.Max(x => x.Value)) + 1}");
+        _questions.Add(new Question(questionId, 1, string.Empty));
+        AddOption(questionId, string.Empty);
 
-        _questionOption.Add(questioOptionMax, questionId);
-        _options.Add(questioOptionMax, optionText);
+        UpdateLPs();
+        StateHasChanged();
+    }
 
+    private void UpdateLPs()
+    {
+        int lpQuestion = 1;
+
+        foreach (var q in _questions)
+        {
+            q.SetLp(lpQuestion);
+
+            int lpOption = 1;
+
+            foreach (var qo in _questionOptions.Where(x => x.GetQuestionId() == q.GetId()))
+            {
+                qo.SetLp(lpOption);
+                lpOption++;
+            }
+
+            lpOption = 1;
+            lpQuestion++;
+        }
+
+        StateHasChanged();
+    }
+
+    private void DeleteQuestion(Question question)
+    {
+        var questions = _questions;
+
+        if (questions.Count > 1)
+        {
+            _questionOptions.RemoveAll(x => x.GetQuestionId() == question.GetId());
+            _questions.Remove(question);
+        }
+
+        UpdateLPs();
+        StateHasChanged();
+    }
+
+    private void AddOption(int questionId, string optionText)
+    {
+        int optionId = _questionOptions.Count == 0 ? 1 : _questionOptions.Max(x => x.GetId()) + 1;
+        _questionOptions.Add(new QuestionOption(optionId, optionId, questionId, optionText));
+
+        UpdateLPs();
+        StateHasChanged();
+    }
+    private void EditOption(int optionId, string optionText)
+    {
+        var questionOption = _questionOptions.FirstOrDefault(x => x.GetId() == optionId);
+        questionOption.SetText(optionText);
+
+        StateHasChanged();
+    }
+
+    private void DeleteOption(QuestionOption option)
+    {
+        var options = _questionOptions.Where(x => x.GetQuestionId() == option.GetQuestionId()).ToList();
+
+        if (options.Count > 1)
+            _questionOptions.Remove(option);
+
+        UpdateLPs();
         StateHasChanged();
     }
 }
---
+++ PI6.Components/Shared/Objects/Question.cs
@@ -0,0 +1,22 @@
+﻿namespace PI6.Components.Shared.Objects;
+
+public class Question
+{
+    private int _id;
+    private int _lp;
+    private string _title;
+
+    public Question(int id, int lp, string title)
+    {
+        _id = id;
+        _lp = lp;
+        _title = title;
+    }
+
+    public int GetId() => _id;
+    public void SetId(int id) => _id = id;
+    public int GetLp() => _lp;
+    public void SetLp(int lp) => _lp = lp;
+    public string GetTitle() => _title;
+    public void SetTitle(string title) => _title = title;
+}
