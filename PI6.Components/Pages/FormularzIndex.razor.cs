82d16abc12d2ecf673e3f31cbe57270bc3ccf24c
Edward Gadawski <edwardgadawski@gmail.com>
2023-03-08 22:54:14 +0100

    yup

--- PI6.Components/Pages/FormularzIndex.razor.cs
+++ PI6.Components/Pages/FormularzIndex.razor.cs
@@ -1,6 +1,13 @@
 ﻿namespace PI6.Components.Pages;
 
-public partial class TworzenieFormularza
+public partial class FormularzIndex
 {
+    private List<int> _list = new();
+    private List<int> _list2 = new();
 
+    protected override void OnInitialized()
+    {
+        _list.Add(1);
+        _list.Add(1);
+    }
 }
