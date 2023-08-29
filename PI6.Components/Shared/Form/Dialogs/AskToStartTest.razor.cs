3b73d458ab4ef386e216f76d086d1528481de02f
Edward Gadawski <edwardgadawski@gmail.com>
2023-08-29 20:33:13 +0200

    Zmiany w wyglądzie strony dla studenta / dodanie dialogu

---
+++ PI6.Components/Shared/Form/Dialogs/AskToStartTest.razor.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace PI6.Components.Shared.Form.Dialogs;
+
+public partial class AskToStartTest
+{
+    [Inject] public NavigationManager NavigationManager { get; set; }
+
+    [CascadingParameter] MudDialogInstance MudDialog { get; set; }
+
+    [Parameter] public string Href { get; set; }
+
+    void Cancel() => MudDialog.Cancel();
+
+    private void SendToSolveTest()
+    {
+        NavigationManager.NavigateTo(Href);
+    }
+}
--- PI6.Components/Shared/FormTilesIndex.razor.cs
+++ PI6.Components/Shared/FormTilesIndex.razor.cs
@@ -1,31 +1,51 @@
 ﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
 using MudBlazor;
+using PI6.Components.Helpers.Interfaces;
 using PI6.Components.Shared.Form.Dialogs;
 using PI6.Shared.Data.Dtos;
+using PI6.Shared.Data.Entities;
+using PI6.WebApi.Services;
 
 namespace PI6.Components.Shared;
 
 public partial class FormTilesIndex
 {
     [Inject] private IDialogService DialogService { get; set; }
+    [Inject] public IApplicationService ApplicationService { get; set; }
+    [Inject] public ISnackbarHelper ErrorHelper { get; set; }
+    [Inject] public IAccountHelper AccountHelper { get; set; }
+
+    [CascadingParameter] private Task<AuthenticationState> authenticationState { get; set; }
 
     [Parameter] public List<FormularzKafelekDto> FormTiles { get; set; }
-    [Parameter] public AccountDto Account { get; set; }
 
+    //private List<formularz_podejscie> _solvedForms;
+    private List<formularz> _forms;
+    private Dictionary<formularz, int> _formsPointsSum;
+
+    private AccountDto _accountDto;
     private bool _isStudent = false;
     private string _href = string.Empty;
 
-    protected override void OnInitialized()
+    protected override async Task OnInitializedAsync()
     {
-        _href = Account.UstName switch
+        if (authenticationState is not null)
         {
-            "Lecturer" => "formdetails",
-            "Student" => "formsolve",
-            "Admin" => "formdetails",
-            _ => "formdetails"
-        };
-
-        _isStudent = Account.UstName == "Student" ? true : false;
+            try
+            {
+                _accountDto = await AccountHelper.LoadAccount(authenticationState, ApplicationService);
+                if (_accountDto is not null)
+                {
+                    await LoadData();
+                    StateHasChanged();
+                }
+            }
+            catch (Exception e)
+            {
+                ErrorHelper.ShowSnackbar(e.Message, Severity.Error, false, true);
+            }
+        }
     }
 
     private void OpenAssignFormDialog(int formId)
@@ -56,5 +76,49 @@ public partial class FormTilesIndex
         };
 
         DialogService.Show<FormDates>(null, parameters, options);
+    }
+
+    private void OpenAskToStartDialog(string href)
+    {
+        var parameters = new DialogParameters
+        {
+            { "Href", href }
+        };
+
+        var options = new DialogOptions { CloseOnEscapeKey = true };
+        DialogService.Show<AskToStartTest>("Czy na pewno chcesz rozpocząć test?", parameters, options);
+    }
+
+    private async Task LoadData()
+    {
+        _forms = new();
+        _formsPointsSum = new();
+
+        _href = _accountDto.UstName switch
+        {
+            "Lecturer" => "formdetails",
+            "Student" => "formsolve",
+            "Admin" => "formdetails",
+            _ => "formdetails"
+        };
+
+        _isStudent = _accountDto.UstName == "Student";
+        //_solvedForms = await ApplicationService.GetSolvedForms(_accountDto.UserId);
+        foreach (var fT in FormTiles)
+        {
+            var form = await ApplicationService.GetForm(fT.ForId);
+            var questions = await ApplicationService.GetFormQuestions(fT.ForId);
+
+            _forms.Add(form);
+            _formsPointsSum.Add(form, questions.Sum(x => x.forp_punkty));
+        }
+    }
+
+    private static string FormatRequiredTime(int time)
+    {
+        if (time < 10)
+            return string.Concat("0", time);
+        else
+            return time.ToString();
     }
 }
